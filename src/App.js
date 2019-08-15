07f27c86a04c62a5c5c04442ae975f80ad6a2314
dhia1452@gmail.com <53936290+Dhia-Bennour@users.noreply.github.com>
2019-08-15 15:46:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Time from './Time'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            timeMs: 0
+        }
+        setInterval(
+            () => {
+               this.setState({
+                   timeMs: this.state.timeMs + 1000
+               }) 
+            },
+            1000
+        )
+    }
+    render() {
+        return <Time ms={this.state.timeMs} />
+    }
 }
 
-export default App;
+export default Timer
